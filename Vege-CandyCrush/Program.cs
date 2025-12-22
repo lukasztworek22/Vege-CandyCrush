695acae2ef81ca4b6494c933e1eb39512d3f1165
Łukasz Tworek <lukasz.tworek22@gmail.com>
2025-12-22 16:06:44 +0100

    I made an 2D array and vegies elemnts inside other 1D array to serve as a FILLING for the 2D array for now and made it so the strings inside the  1D array go into 2D array randomly

--- Vege-CandyCrush/Program.cs
+++ Vege-CandyCrush/Program.cs
@@ -1,3 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using System;
 
-Console.WriteLine("Hello, World!");
+
+class Program
+{
+    
+    static void Main(string[] args)
+    {
+        // for now I created normal string arrays, I assume that later they need to be separate objects?
+        string[] vegies = {"kale", "beet", "leek", "okra"};
+        string[] fruits = {"date", "plum", "pear", "kiwi"};
+        string[,] board = new string[12,12];
+        
+        Random rnd = new Random();
+        for (int i = 0; i < board.GetLength(0); i++)
+        {
+            for (int j = 0; j < board.GetLength(1); j++)
+            {
+                board[i, j] = vegies[rnd.Next(vegies.Length)];
+            }
+        }
+        for (int i = 0; i < board.GetLength(0); i++)
+        {
+            for (int j = 0; j < board.GetLength(1); j++)
+            {
+                Console.Write(board[i, j] + "    ");
+            }
+            Console.WriteLine();
+            Console.WriteLine();
+        }
+    }
+}
