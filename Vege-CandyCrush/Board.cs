1756356943ddb87ba5a72206adae51c5ba0b255b
Łukasz Tworek <lukasz.tworek22@gmail.com>
2026-03-12 19:32:51 +0100

    Validiation , filling

---
+++ Vege-CandyCrush/Board.cs
@@ -0,0 +1,100 @@
+﻿namespace Vege_CandyCrush;
+
+public class Board
+{
+    public string[,] Content { get; set; }
+    
+    public Board(int width, int height)
+    {
+        Content = new string[height, width];
+    }
+    
+    public void PrintBoard() 
+    {
+        for (int j = 0; j < Content.GetLength(0); j++)
+        {
+            for (int i = 0; i < Content.GetLength(1); i++)
+            {
+                string symbol = Content[j, i] ?? ".";
+                Console.Write(symbol.PadLeft(3));
+            }
+            Console.WriteLine();
+        }
+    }
+    public void ElementSwap()
+    {
+        
+        
+        Console.WriteLine("Enter the first coordinate: ");
+        string E1 = Console.ReadLine().ToUpper(); // first element you want to move
+        
+        if (!CorrectCoordinate(E1, out int elemRow1, out int elemCol1))
+        {
+            Console.WriteLine("Invalid coordinate! Press any key to try again...");
+            Console.ReadKey();
+            return;
+        }
+        Console.WriteLine("Enter the second coordinate: ");
+        string E2 = Console.ReadLine().ToUpper(); // element you want switch places with
+        
+        if (!CorrectCoordinate(E2, out int elemRow2, out int elemCol2))
+        {
+            Console.WriteLine("Invalid coordinate! Press any key to try again...");
+            Console.ReadKey();
+            return;
+        }
+        
+        //Move Validation
+        int rowDiff = Math.Abs(elemRow1 - elemRow2);
+        int colDiff = Math.Abs(elemCol1 - elemCol2);
+        
+        if (rowDiff + colDiff != 1)
+        {
+            Console.WriteLine("Incorrect move!!  Press any key to try again.....");
+            Console.ReadKey();
+            return;
+        }
+        // Swap
+        string temp = Content[elemRow1, elemCol1];
+        Content[elemRow1, elemCol1] = Content[elemRow2, elemCol2];
+        Content[elemRow2, elemCol2] = temp;
+        
+       
+    }
+
+    private bool CorrectCoordinate(string input, out int row, out int col)
+    {
+        row = col = -1;
+        if (string.IsNullOrWhiteSpace(input))
+        {
+            return false;
+        }
+
+        input = input.Trim().ToUpper();
+
+        if (input.Length < 2)
+        {
+            return false;
+        }
+
+        char letter = input[0];
+        if (letter < 'A' || letter >= 'A' + Content.GetLength(1))
+        {
+            return false;
+        }
+
+        if (!int.TryParse(input.Substring(1), out int number))
+        {
+            return false;
+        }
+
+        row = number;
+        col = (letter - 'A') +1;
+
+        if (row < 0 || row >= Content.GetLength(0))
+        {
+            return false;
+        }
+        return true;
+    }
+}
---
+++ Vege-CandyCrush/GameManager.cs
@@ -0,0 +1,204 @@
+﻿namespace Vege_CandyCrush;
+
+public class GameManager
+{
+    private Board board;
+    private BoardFiller filler;
+    
+    public GameManager(Board board, BoardFiller filler)
+    {
+        this.board = board;
+        this.filler = filler;
+    }
+    
+    public List<(int row, int col)> FindMatchesThree()
+    {
+        List<(int, int)> matches = new List<(int, int)>();
+        int rows = board.Content.GetLength(0);
+        int cols = board.Content.GetLength(1);
+
+        for (int i = 1; i < rows; i++)
+        {
+            for (int j = 1; j < cols - 2; j++)
+            {
+                if (board.Content[i, j] == board.Content[i, j + 1] && board.Content[i, j] == board.Content[i, j + 2])
+                {
+                    matches.Add((i, j));
+                    matches.Add((i, j + 1));
+                    matches.Add((i, j + 2));
+                    
+                }
+            }
+        }
+
+        for (int j = 1; j < cols; j++)
+        {
+            for (int i = 1; i < rows - 2; i++)
+            {
+                if (board.Content[i, j] == board.Content[i + 1, j] && board.Content[i, j] == board.Content[i + 2, j])
+                {
+                    matches.Add((i, j));
+                    matches.Add((i + 1, j));
+                    matches.Add((i + 2, j));
+                    
+                }
+            }
+        }
+
+        return matches.Distinct().ToList();
+    }
+
+    public void MatchesExplosion(List<(int row , int col )> matches)
+    {
+        foreach (var (row, col) in matches)
+        {
+            board.Content[row, col] = null;
+        }
+    }
+
+    public void ApplyGravity(FillDirection direction)
+    {
+        switch (direction)
+        {
+            case FillDirection.Top:
+                ApplyGravityDown();
+                break;
+            case FillDirection.Bottom:
+                ApplyGravityUp();
+                break;
+            case FillDirection.Left:
+                ApplyGravityRight();
+                break;
+            case FillDirection.Right:
+                ApplyGravityLeft();
+                break;
+        }
+        
+    }
+
+    
+    public void Prepare()
+    {
+        filler.VegieFill();
+        filler.FillHeaders();
+    }
+
+    public void ApplyGravityDown()
+    {
+        int rows = board.Content.GetLength(0);
+        int cols = board.Content.GetLength(1);
+
+
+        for (int col = 1; col < cols; col++)
+        {
+            int writeRow = rows - 1;
+            for (int row = rows - 1; row >= 1; row--)
+            {
+                if (board.Content[row, col] != null)
+                {
+                    board.Content[writeRow, col] = board.Content[row, col];
+                    if (writeRow != row)
+                    {
+                        board.Content[row, col] = null;
+                    }
+
+                    writeRow--;
+                }
+            }
+
+            for (; writeRow >= 1; writeRow--)
+            {
+                board.Content[writeRow, col] = null;
+            }
+        }
+    }
+    public void ApplyGravityUp()
+    {
+        int rows = board.Content.GetLength(0);
+        int cols = board.Content.GetLength(1);
+
+
+        for (int col = 1; col < cols; col++)
+        {
+            int writeRow = 1;
+            for (int row = 1; row <rows ; row++)
+            {
+                if (board.Content[row, col] != null)
+                {
+                    board.Content[writeRow, col] = board.Content[row, col];
+                    if (writeRow != row)
+                    {
+                        board.Content[row, col] = null;
+                    }
+
+                    writeRow++;
+                }
+            }
+
+            for (; writeRow < rows; writeRow++)
+            {
+                board.Content[writeRow, col] = null;
+            }
+        }
+    }
+    public void ApplyGravityRight()
+    {
+        int rows = board.Content.GetLength(0);
+        int cols = board.Content.GetLength(1);
+
+
+        for (int row = 1; row < rows; row++)
+        {
+            int writeCol = cols - 1;
+            for (int col = cols -1; col >= 1; col--)
+            {
+                if (board.Content[row, col] != null)
+                {
+                    board.Content[row, writeCol] = board.Content[row, col];
+                    if (writeCol != col)
+                    {
+                        board.Content[row, col] = null;
+                    }
+
+                    writeCol--;
+                }
+            }
+
+            for (; writeCol >= 1; writeCol--)
+            {
+                board.Content[row, writeCol] = null;
+            }
+        }
+    }
+    
+    public void ApplyGravityLeft()
+    {
+        int rows = board.Content.GetLength(0);
+        int cols = board.Content.GetLength(1);
+
+
+        for (int row = 1; row < rows; row++)
+        {
+            int writeCol = 1;
+            for (int col = 1; col < cols; col++)
+            {
+                if (board.Content[row, col] != null)
+                {
+                    board.Content[row, writeCol] = board.Content[row, col];
+                    if (writeCol != col)
+                    {
+                        board.Content[row, col] = null;
+                    }
+
+                    writeCol++;
+                }
+            }
+
+            for (; writeCol < cols; writeCol++)
+            {
+                board.Content[row, writeCol] = null;
+            }
+        }
+    }
+    
+}
